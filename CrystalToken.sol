5b270ef93a691c5e8f0974b6d3bf14a6142a196d
Pawan94580 <pawanpanday277@gmail.com>
2024-09-01 00:31:52 +0530

    Create CrystalToken.sol

---
+++ CrystalToken.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "./ICrystalBurnable.sol";
+
+contract CrystalToken is ERC20, ICrystalBurnable {
+    constructor() ERC20("Crystal", "CRY") {
+        _mint(msg.sender, 10**18);
+    }
+
+    function decimals() public pure override returns (uint8) {
+        return 0;
+    }
+
+    function burn(uint amount, address account) public override {
+        _burn(account, amount);
+    }
+}
